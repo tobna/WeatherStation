a719fd0d79833f7c677336ec5268710b5f3a86c4
Tobias Nauen <tobias.nauen@gmail.com>
2021-12-28 00:55:52 +0100

    try hovering

--- make_webpage.py
+++ make_webpage.py
@@ -13,12 +13,36 @@ from read_sensors import _TABLE_NAME
 _HTML_FOLDER = '/home/pi/WeatherStation/html/'
 
 
-def make_plot(dates, data, title, ylabel, filename):
+def make_plot(dates, data, title, unit, filename):
     fig = plt.figure()
-    plt.plot(dates, data)
+    l, = plt.plot(dates, data, label=[f"{date}; {datum} {unit}" for date, datum in zip(dates, data)])
     plt.title(title)
     plt.xlabel("Date")
-    plt.ylabel(ylabel)
+    plt.ylabel(f"{title} [{unit}]")
+
+    annot = plt.annotate("", xy=(0, 0), xytext=(20, 20), textcoords="offset points",
+                         bbox=dict(boxstyle="round", fc="w"), arrowprops=dict(arrowstyle="->"))
+    annot.set_visible(False)
+
+    def hover(event):
+        vis = annot.get_visible()
+        cont, ind = l.contains(event)
+        idx = ind['ind'][0]
+        if cont:
+            posx, posy = [l.get_xdata()[idx], l.get_ydata()[idx]]
+            annot.xy = (posx, posy)
+            text = f'{l.get_label()}: {posx:.2f}-{posy:.2f}'
+            annot.set_text(text)
+            # annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
+            annot.get_bbox_patch().set_alpha(0.4)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
+    fig.canvas.mpl_connect("motion_notify_event", hover)
+
     html_str = mpld3.fig_to_html(fig)
     with open(_HTML_FOLDER + filename, 'w+') as f:
         f.write(html_str)
@@ -27,8 +51,8 @@ def make_plot(dates, data, title, ylabel, filename):
 def main(cur):
     cur.execute(f"SELECT time, temp, hum FROM {_TABLE_NAME};")
     times, temps, hums = zip(*cur)
-    make_plot(times, temps, "Temperature", "Temperature [°C]", "temp.html")
-    make_plot(times, hums, "Humidity", "Humidity [%]", "hum.html")
+    make_plot(times, temps, "Temperature", "°C", "temp.html")
+    make_plot(times, hums, "Humidity", "%", "hum.html")
 
 
 if __name__ == '__main__':
