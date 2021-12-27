f9c908b6bec62f4013913da891ab0027a6d97504
Tobias Nauen <tobias.nauen@gmail.com>
2021-12-27 15:14:54 +0100

    different way to call i2c

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import Adafruit_DHT
 import adafruit_ccs811
 import busio
-import board
+from board import *
 import argparse
 from time import time, sleep
 import logging as log
@@ -38,7 +38,7 @@ def read_dht22(temp_data_pin):
 
 
 def read_ccs811():
-    i2c_bus = busio.I2C(board.SCL, board.SDA)
+    i2c_bus = board.I2C()
     ccs811 = adafruit_ccs811.CCS811(i2c_bus)
     while not ccs811.data_ready:
         pass
