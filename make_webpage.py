3947280a6684b370cc6daa74be2d5ebb09887a7a
Tobias Nauen <tobias.nauen@gmail.com>
2021-12-28 04:04:50 +0100

    plotting via plotly

--- make_webpage.py
+++ make_webpage.py
@@ -1,37 +1,54 @@
 #!/usr/bin/env python
 
-from matplotlib import pyplot as plt
-from matplotlib import dates
-import mpld3
 import argparse
 import logging as log
 import mysql.connector as mariadb
 import sys
 from read_sensors import _TABLE_NAME
+from math import ceil, floor
+import plotly.express as px
+from pandas import DataFrame
+
+
+def down_to(x, base):
+    return base * floor(x / base)
+
+
+def up_to(x, base):
+    return base * ceil(x / base)
 
 
 _HTML_FOLDER = '/home/pi/WeatherStation/html/'
 
 
 def make_plot(dates, data, title, unit, filename):
-    fig = plt.figure()
-    l, = plt.plot(dates, data, label=[f"{date}; {datum} {unit}" for date, datum in zip(dates, data)])
-    plt.title(title)
-    plt.xlabel("Date")
-    plt.ylabel(f"{title} [{unit}]")
+    df = DataFrame(data=[dates, data]).transpose()
+    df.columns = ['Date', title]
+    fig = px.line(df, x='Date', y=title, title=title, template='plotly_dark', hover_data={"Date": "|%d.%m. %H:%M"})
 
-    def on_mouse_move(event):
-        plt.vlines = []
-        plt.axvline(x=event.xdata, color='k')
-
-    fig.canvas.mpl_connect('motion_notify_event', on_mouse_move)
-    html_str = mpld3.fig_to_html(fig)
-    with open(_HTML_FOLDER + filename, 'w+') as f:
-        f.write(html_str)
+    fig.update_traces(hovertemplate="%{y}" + unit + " @ %{x|%d.%m. %H:%M}")
+    fig.update_layout(hoverdistance=1000, hovermode='x')
+    fig.update_xaxes(rangeslider={"visible": True, "bgcolor": "#0d0d0d"}, rangeselector={
+        "buttons": [{"label": "all time", "step": "all"},
+                    {"count": 1, "label": "year", "step": "year", "stepmode": "backward"},
+                    {"count": 1, "label": "month", "step": "month", "stepmode": "backward"},
+                    {"count": 7, "label": "week", "step": "day", "stepmode": "backward"},
+                    {"count": 1, "label": "day", "step": "day", "stepmode": "backward"}],
+        "bgcolor": "#888", "activecolor": "#444"},
+                     showspikes=True, spikesnap="cursor", spikemode="across",
+                     tickformatstops=[{"dtickrange": [None, 60000], "value": "%H:%M:%S"},
+                                      {"dtickrange": [60000, 36000000], "value": "%H:%M\n%d.%m.%Y"},
+                                      {"dtickrange": [36000000, "M1"], "value": "%d.%m.%Y"},
+                                      {"dtickrange": ["M1", "M12"], "value": "%m.%Y"},
+                                      {"dtickrange": ["M12", None], "value": "%Y"}])
+    fig.update_yaxes(range=[down_to(min(data), 5), up_to(max(data), 5)])
+    fig.write_html(_HTML_FOLDER + filename, config={"displayModeBar": False, "showTips": False})
 
 
 def main(cur):
     cur.execute(f"SELECT time, temp, hum FROM {_TABLE_NAME};")
+    """cur = [(datetime(2021, 12, 27, 17, 0, 0, 0), 21.4, 41.5), (datetime(2021, 12, 27, 18, 0, 0, 0), 19.8, 43.),
+           (datetime(2021, 12, 27, 21, 0, 0, 0), 17.9, 42.), (datetime(2021, 12, 28, 1, 0, 0, 0), 18.7, 44.)]"""
     times, temps, hums = zip(*cur)
     make_plot(times, temps, "Temperature", "°C", "temp.html")
     make_plot(times, hums, "Humidity", "%", "hum.html")
